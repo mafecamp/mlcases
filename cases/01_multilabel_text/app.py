dc10f7300dad70abb3c65894cd163cf56992c538
mafecamp <nandacampanari@gmail.com>
2024-03-18 13:00:00 -0300

    add config, download do modelo

---
+++ cases/01_multilabel_text/app.py
@@ -0,0 +1,125 @@
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+)
+
+from loguru import logger
+from config import settings
+import numpy as np
+import torch
+import argparse
+import gdown
+import os
+
+
+def parse_labels(labels: list):
+    """
+    Parses the labels from the settings file.
+    """
+    id2label = {}
+
+    for idx, label in enumerate(labels):
+        id2label[idx] = label
+
+    label2id = {v: k for k, v in id2label.items()}
+
+    return id2label, label2id
+
+
+parser = argparse.ArgumentParser(
+    prog="multilabel text utilities", description="Process some integers."
+)
+parser.add_argument(
+    "--input-type", type=str, help="The input type to be used", default="phrase"
+)
+parser.add_argument("phrase", type=str, help="The phrase to be classified")
+parser.add_argument("threshold", type=float, help="The threshold to be used")
+
+parsed_args = parser.parse_args()
+input_type = parsed_args.input_type
+
+if parsed_args.input_type == "phrase":
+    phrase = parsed_args.phrase
+else:
+    raise ValueError(f"Input type {parsed_args.input_type} not supported")
+
+threshold = parsed_args.threshold
+labels = settings.prediction.labels
+id2label, label2id = parse_labels(labels)
+
+# id2label = {
+#     0: "educação",
+#     1: "finanças",
+#     2: "indústrias",
+#     3: "orgão público",
+#     4: "varejo",
+# }
+
+# label2id = {
+#     "educação": 0,
+#     "finanças": 1,
+#     "indústrias": 2,
+#     "orgão público": 3,
+#     "varejo": 4,
+# }
+
+if not os.path.exists(settings.trained_model_path):
+    # Baixa o modelo a primeira vez caso não exista
+    logger.info("Baixando o modelo pré-treinado padrão pela primeira vez...")
+    file_id = "1MfyowP9czztTx58dRn6xSFTNbwR2CmnL"
+    gdown.download(id=file_id, output="model.zip", quiet=False)
+    logger.info("Descompactando o modelo...")
+    os.system(f"unzip model.zip -d ./models/")
+
+new_model = AutoModelForSequenceClassification.from_pretrained(
+    settings.trained_model_path,
+    problem_type="multi_label_classification",
+    num_labels=len(labels),
+    id2label=id2label,
+    label2id=label2id,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    pretrained_model_name_or_path=settings.base_model_name
+)
+
+
+def predict_phrase(phrase):
+    """
+    Prevê uma frase dado um modelo e um tokenizador.
+    """
+    inputs = tokenizer(
+        phrase, return_tensors="pt", padding=True, truncation=True, max_length=128
+    )
+
+    outputs = new_model(**inputs)
+
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(outputs.logits.squeeze().cpu())
+    predictions = np.zeros(probs.shape)
+    predictions[np.where(probs >= threshold)] = 1
+    predicted_labels = [
+        id2label[idx] for idx, label in enumerate(predictions) if label == 1.0
+    ]
+
+    return predicted_labels
+
+
+# create string from list itens
+if input_type == "phrase":
+    predicted_labels = predict_phrase(phrase)
+    labels_str = ", ".join(predicted_labels)
+    logger.info(f"Rótulos: {labels_str}")
+elif input_type == "file":
+    with open(phrase, "r") as f:
+        for line in f:
+            predicted_labels = predict_phrase(line)
+            logger.info(f"Predicted labels: {predicted_labels}")
+
+            # save as predictions
+            with open("predictions.txt", "a") as f:
+                f.write(f"{line.strip()} - {predicted_labels}\n")
+else:
+    raise ValueError(
+        f"Input type {input_type} not supported. Use one of: 'phrase' or 'file'"
+    )
